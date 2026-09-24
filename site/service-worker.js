06d7eccdac7f3a4b6354d29a8442bb925ce487b9
zhaotools <rayzwg@gmail.com>
2026-09-24 23:02:33 +0800

    fix: refresh navigation and favicon caches

--- site/service-worker.js
+++ site/service-worker.js
@@ -1,14 +1,14 @@
-const CACHE = "lz-assetscope-v0.7.0";
+const CACHE = "lz-assetscope-v0.7.1";
 const SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=0.7.0",
-  "./app.js?v=0.7.0",
+  "./app.js?v=0.7.1",
   "./manifest.webmanifest",
-  "./favicon.ico?v=0.6.3",
-  "./icons/icon-32.png?v=0.6.3",
-  "./icons/icon-180.png?v=0.6.3",
-  "./icons/icon-192.png?v=0.6.3",
+  "./favicon.ico?v=0.7.1",
+  "./icons/icon-32.png?v=0.7.1",
+  "./icons/icon-180.png?v=0.7.1",
+  "./icons/icon-192.png?v=0.7.1",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
 ];
